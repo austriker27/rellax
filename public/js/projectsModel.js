6cfe55c126493b5434a8502f12ea0415a282f95d
austriker27 <davidalindahl@gmail.com>
2017-10-24 16:41:42 -0700

    updates to projects constructor - trying to get handlebars to work

--- public/js/projectsModel.js
+++ public/js/projectsModel.js
@@ -1,12 +1,29 @@
 'use strict';
 
 
-  // Project.prototype.toHtml = function() {
-  //   let sourceHTML = $('#project-template').html();
-  //   let actualTemplate = Handlebars.compile(sourceHTML);
-  //   $('.projectsAnchor').append(Project.projects.map(actualTemplate));
-  // };
 
-  let sourceHTML = $('projects-template').html();
-  let compiledTemplate = Handlebars.compile(sourceHTML);
-  $('.projects-anchor').append(map(compiledTemplate));
+(function(module) {
+  function Project (projects) {
+    this.title = projects.title;
+    this.url = projects.url;
+    this.img = projects.img;
+    this.projectDescription = projects.projectDescription;
+  }
+
+  projects.all = [];
+
+  Project.prototype.toHtml = function() {
+    let sourceHTML = $('#projects-template').html();
+    let compiledTemplate = Handlebars.compile(sourceHTML);
+    return compiledTemplate(this);
+    // $('#projects-anchor').append(compiledTemplate);
+  }
+
+  rawData.forEach(function(projectObject) {
+    projects.all.push(new Project(projectObject));
+  });
+
+  projects.all.forEach(function(project){
+    $('#projects-anchor').append(project.toHtml());
+  });
+};
