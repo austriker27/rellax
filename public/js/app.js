05c8d63c1fd0fc11cd2da8b9844135d2ad78d247
austriker27 <davidalindahl@gmail.com>
2017-10-24 13:31:25 -0700

    added brief bios and still trying to fix parallax library

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
-  var rellax = new Rellax('.rellax', {
-    // center: true
-    callback: function(position) {
-        // callback every position change
-        console.log(position);
-    }
-  });
+// var rellax = new Rellax('.rellax', {
+//   // center: true
+//   callback: function(position) {
+//       // callback every position change
+//       console.log(position);
+//   }
+// });
