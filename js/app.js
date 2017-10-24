160e39eea240a9dcb29f2410f6cd4e33af9c537a
austriker27 <davidalindahl@gmail.com>
2017-10-24 14:12:08 -0700

    moved everything back into public folder bc parallax works and im dumb

--- js/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// var rellax = new Rellax('.rellax', {
-//   // center: true
-//   callback: function(position) {
-//       // callback every position change
-//       console.log(position);
-//   }
-// });
