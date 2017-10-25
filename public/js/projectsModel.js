2a2c79b87bbf50071e554c8a296122bd136a5647
austriker27 <davidalindahl@gmail.com>
2017-10-24 18:12:18 -0700

    got handlebars using each helper class working! woot!

--- public/js/projectsModel.js
+++ public/js/projectsModel.js
@@ -1,29 +1,43 @@
 'use strict';
 
+let $sourceHTML = $('#projects-template').html();
+let $compiledTemplate = Handlebars.compile($sourceHTML);
+var $newHTML = $($compiledTemplate({
+  project: [
+    {
+      title: 'fraction calculator | ios app',
+      url: `http://#.com`,
+      // img: `../imgs/fractionCalc-mockup.jpg`,
+      projectDescription: 'here are some words about the project and how we made it awesome.'
+    }
+  ]
+}));
+$('#projects-anchor').append($newHTML);
 
+// TODO: pass in objects from JSON into the above function rather than manual coding it.
 
 // (function(module) {
-  function Project (projects) {
-    this.title = projects.title;
-    this.url = projects.url;
-    this.img = projects.img;
-    this.projectDescription = projects.projectDescription;
-  }
-
-  projects.all = [];
-
-  Project.prototype.toHtml = function() {
-    let sourceHTML = $('#projects-template').html();
-    let compiledTemplate = Handlebars.compile(sourceHTML);
-    return compiledTemplate(this);
-    // $('#projects-anchor').append(compiledTemplate);
-  }
-
-  rawData.forEach(function(projectObject) {
-    projects.all.push(new Project(projectObject));
-  });
-
-  projects.all.forEach(function(project){
-    $('#projects-anchor').append(project.toHtml());
-  });
+  // function Project (projects) {
+  //   this.title = projects.title;
+  //   this.url = projects.url;
+  //   this.img = projects.img;
+  //   this.projectDescription = projects.projectDescription;
+  // }
+  //
+  // projects.all = [];
+  //
+  // Project.prototype.toHtml = function() {
+  //   let sourceHTML = $('#projects-template').html();
+  //   let compiledTemplate = Handlebars.compile(sourceHTML);
+  //   return compiledTemplate(this);
+  //   // $('#projects-anchor').append(compiledTemplate);
+  // }
+  //
+  // rawData.forEach(function(projectObject) {
+  //   projects.all.push(new Project(projectObject));
+  // });
+  //
+  // projects.all.forEach(function(project){
+  //   $('#projects-anchor').append(project.toHtml());
+  // });
 // };
